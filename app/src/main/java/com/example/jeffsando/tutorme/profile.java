9bc71dd1e341fa39a704f2f8dee4f77a1ecd0390
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2018-11-04 11:16:12 -0600

    final algo connecting sessions to dates/courses per student

--- app/src/main/java/com/example/jeffsando/tutorme/profile.java
+++ app/src/main/java/com/example/jeffsando/tutorme/profile.java
@@ -17,12 +17,25 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 
+import java.util.*;
+
+
 
 public class profile extends AppCompatActivity {
-
     private DatabaseReference mDatabase;
     private FirebaseAuth auth;
     String currUsername = "";
+    ArrayList<String> usersCoursesList = new ArrayList<String>();
+    ArrayList<String> usersDaysAvailability = new ArrayList<String>();
+
+
+    ArrayList<HashMap<String, String>> allCoursesDataList = new ArrayList<HashMap<String, String>>();
+
+    ArrayList<HashMap<String, String>> matchingCourses = new ArrayList<HashMap<String, String>>();  //do matching times list next
+
+    ArrayList<HashMap<String, String>> matchingCoursesAndTime = new ArrayList<HashMap<String, String>>();  //do matching times list next
+
+    ArrayList<String> allCoursesAsText = new ArrayList<String>();
 
 
     @Override
@@ -69,7 +82,253 @@ public class profile extends AppCompatActivity {
         //TODO: Enable button to search job board based on
         //Intent intent = new Intent(this,job_board.class);
         //startActivity(intent);
+
+
+        //Get the data from your courses, grab all the courses or whatever
+
+
+        getUserCourses();
+        getUserAvailability();
+
+        getAllSessions(); //don't have time to do exact queries right away
+
+
+
+        //////TODO: when we present the pages we can just do a string builder.
+
+
+        getMatchingSessions();
+
+        getMatchingDays();
+
+
+
+
     }
+
+
+
+    public void getAllSessions() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("tutoring sessions");
+        ref.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+                        getHashedData((Map<String,Object>) dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
+
+
+
+
+    }
+
+
+    public void getHashedData(Map<String, Object> sessionData)
+    {
+        //allCourseData //= new ArrayList<HashMap<String, String>>();
+
+        for (Map.Entry<String, Object> entry : sessionData.entrySet()) {
+
+            HashMap<String, String> currSession = (HashMap<String, String>) entry.getValue();
+
+
+            allCoursesDataList.add(currSession);
+
+        }
+
+    }
+
+
+
+
+
+    public void getMatchingSessions() {
+
+        for(String course : usersCoursesList) {
+
+
+            for(int i = 0; i < allCoursesDataList.size(); i++) {
+
+                HashMap<String, String> currentCourseData = allCoursesDataList.get(i);
+
+                String courseInData = currentCourseData.get("Course");
+
+                if(course.equals(courseInData)) {  //if they match
+
+                    matchingCourses.add(currentCourseData);
+
+                }
+
+
+
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+    public void getMatchingDays() {
+
+        for(String day : usersDaysAvailability) {
+
+            for(int i = 0; i < matchingCourses.size(); i++) {
+
+                HashMap<String, String> currMatchCourseData = matchingCourses.get(i);
+
+                String sessionDay = currMatchCourseData.get("Date");
+
+                if(day.equals(sessionDay)) {
+
+                    matchingCoursesAndTime.add(currMatchCourseData);
+                }
+
+
+            }
+
+
+        }
+
+
+
+        for(int i = 0; i < matchingCoursesAndTime.size(); i++)
+        {
+            HashMap<String, String> currMatchCourseData = matchingCoursesAndTime.get(i);
+            StringBuilder matchingSessionsAsTest = new StringBuilder();
+
+            matchingSessionsAsTest.append("Tutoring is available for you this week for the course ");
+            matchingSessionsAsTest.append(currMatchCourseData.get("Course"));
+
+            matchingSessionsAsTest.append(". The session's description is: \n ");
+            matchingSessionsAsTest.append(currMatchCourseData.get("Description"));
+
+            matchingSessionsAsTest.append(".\n We are letting you know because it is on one of your free days, ");
+            matchingSessionsAsTest.append(currMatchCourseData.get("Date"));
+
+            matchingSessionsAsTest.append(".\n The rate for this session will be ");
+            matchingSessionsAsTest.append(currMatchCourseData.get("Rate"));
+
+
+
+            matchingSessionsAsTest.append(" dollars, and the time it'll be at is ");
+            matchingSessionsAsTest.append(currMatchCourseData.get("Time"));
+
+
+            allCoursesAsText.add(matchingSessionsAsTest.toString());
+            System.out.println(matchingSessionsAsTest.toString());
+
+//            for (Map.Entry<String, String> entry : currMatchCourseData.entrySet()) {
+//
+//
+//
+//                Log.d("the key is ", entry.getKey());
+//                Log.d("the value is ", entry.getValue());
+//
+//
+//            }
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+    public void getUserCourses() {
+        final String userId = auth.getCurrentUser().getUid();
+
+        mDatabase.child("users").child(userId).child("Courses").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        HashMap<String, String> userCourses = new HashMap<String, String>();
+
+                        userCourses = (HashMap<String, String>) dataSnapshot.getValue();
+
+
+
+                        for (Map.Entry<String, String> item : userCourses.entrySet()) {
+                            String key = item.getKey();
+                            String value = item.getValue();
+
+                            usersCoursesList.add(value);
+                            Log.d("values dont count ", value);
+
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w("getUser:onCancelled", databaseError.toException());
+                        Log.d("shit failed", "db error");
+
+                    }
+                }
+        );
+
+
+    }
+
+
+
+
+
+    public void getUserAvailability() {
+        final String userId = auth.getCurrentUser().getUid();
+
+        mDatabase.child("users").child(userId).child("DaysOpenForTutoring").child("daysAvailable").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        String userAvailability;
+
+                        userAvailability = (String) dataSnapshot.getValue();
+
+                        List<String> finalString= Arrays.asList(userAvailability.split("\\s*,\\s*"));
+
+                        usersDaysAvailability.addAll(finalString);
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w("getUser:onCancelled", databaseError.toException());
+                        Log.d("shit failed", "db error");
+
+                    }
+                }
+        );
+
+
+
+    }
+
+
 
 
     public String updateUsername() {
@@ -112,7 +371,7 @@ public class profile extends AppCompatActivity {
 
                     }
                 }
-                );
+        );
         // [END single_value_read]
 
         Log.d("end updateusername", "function");
